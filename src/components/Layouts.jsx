a292e1456b1ab31e0854e2ae217d7f2cc223b1da
Kurniawan Pratama <kurama.career@gmail.com>
2025-01-11 06:47:36 +0700

    Day 2 - Modified || Fixed || Feature: Perbaikan pada cara menampilkan list menu || perbaikan pada cara pemanggilan list menu agar lebih dinamis || penambahan fitur tag title yang responsive

--- src/components/Layouts.jsx
+++ src/components/Layouts.jsx
@@ -1,56 +1,67 @@
 // DEPENDENCIES
 import { MdMenu } from "react-icons/md";
 import { useState } from "react";
-import { Outlet } from "react-router";
-
+import { NavLink, Outlet, useLocation, useNavigate } from "react-router";
+import { Helmet } from "react-helmet";
 // COMPONENTS
 
 // STYLES
 import "../assets/styles/component_styles/layouts.css";
+import List from "./List";
 
 const Layouts = () => {
   const [isShowNav, setIsShowNav] = useState(false);
-  const [isAuth, setIsAuth] = useState(!false);
+  const [isAuth, setIsAuth] = useState(false);
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
 
   const handleShowNav = () => setIsShowNav(!isShowNav);
+  const buttonListCollection = [
+    { is_auth: null, label: "about" },
+    { is_auth: false, label: "login" },
+    { is_auth: false, label: "register" },
+    { is_auth: true, label: "create" },
+    { is_auth: true, label: "collection" },
+    { is_auth: true, label: "profile" },
+    { is_auth: true, label: "logout" },
+  ];
+  const handleTitle = () => {
+    if (pathname === "/") return "Home";
+
+    return pathname
+      .split("/")[1]
+      .split("")
+      .map((letter, letterIndex) => (letterIndex === 0 ? letter.toUpperCase() : letter.toLowerCase()))
+      .join("");
+  };
   return (
     <>
+      <Helmet>
+        <title>{`Nesting || ${handleTitle()}`}</title>
+      </Helmet>
+
       <header className="header flex relative">
-        <h1 className="web-heading flex">
-          <span>Nesting</span>
+        <div className="web-heading flex">
+          <h1>
+            <NavLink to="/" style={{ color: "inherit", textDecoration: "none" }}>
+              Nesting
+            </NavLink>
+          </h1>
           <span>V.1.0</span>
-        </h1>
+        </div>
         <button type="button" onClick={handleShowNav} className="btn-menu flex absolute">
           <MdMenu />
         </button>
         <nav className={`header-nav flex ${isShowNav || "hidden"}`}>
-          <button type="button" className="btn-redirect">
-            About
-          </button>
-          {isAuth ? (
-            <>
-              <button type="button" className="btn-redirect">
-                Create
-              </button>
-              <button type="button" className="btn-redirect">
-                Collection
-              </button>
-              <button type="button" className="btn-redirect">
-                Profile
-              </button>
-              <button type="button" className="btn-redirect">
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-              <button type="button" className="btn-redirect">
-                Login
-              </button>
-              <button type="button" className="btn-redirect">
-                Register
-              </button>
-            </>
+          {buttonListCollection.map(
+            (value) =>
+              (value.is_auth === isAuth || value.is_auth === null) && (
+                <List
+                  label={value.label}
+                  className={pathname.includes(value.label) ? "highlight-list" : "normal"}
+                  onClick={() => navigate(`/${value.label}`)}
+                />
+              )
           )}
         </nav>
       </header>
