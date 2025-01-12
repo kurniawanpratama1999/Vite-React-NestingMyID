eb5ab98eb11d23ff927f08025c1bd9346d1069fa
Kurniawan Pratama <kurama.career@gmail.com>
2025-01-13 02:32:01 +0700

    Day 3 - [MODIFIED]: Perubahan cara penanganan routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,27 +2,18 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 
-// COMPONENTS
+// LAYOUTS
 import Layouts from "./components/Layouts";
-import ProtectedPage from "./Middleware/ProtectedPage";
-
-// PAGES
-import Home from "./pages/00-Home-all/Home";
 
 // STYLES
 import "./assets/styles/main.css";
+import CreateRoute from "./routes/CreateRoute";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Layouts />}>
-        <Route path="" element={<Home />} /> {/* 00-HOME */}
-        <Route path="about" /> {/* 01-ABOUT */}
-        <Route path="register" /> {/* 02-REGISTER */}
-        <Route path="login" /> {/* 03-LOGIN */}
-        <Route element={<ProtectedPage />}>
-          <Route path="" />
-        </Route>
+        {CreateRoute}
       </Route>
     </Routes>
   </BrowserRouter>
