ae12d0c8c618b81415b71d17ec153da141995d6e
Kurniawan Pratama <kurama.career@gmail.com>
2025-01-13 02:32:01 +0700

    Day 3 - [NEW_FILE]

---
+++ src/components/Input.jsx
@@ -0,0 +1,58 @@
+import "../assets/styles/component_styles/input.css";
+
+const Input = ({
+  children,
+  element = "input",
+  htmlFor = "input-1",
+  type = "text",
+  classLabel = "focus-blue",
+  classInput = "",
+  ...props
+}) => {
+  return (
+    <label htmlFor={htmlFor} className={`label grid relative ${classLabel}`.trim()}>
+      {element === "input" ? (
+        <>
+          <input
+            type={type}
+            id={htmlFor}
+            className={`input grow ${classInput}`}
+            name={htmlFor}
+            spellCheck="false"
+            autoComplete="off"
+            {...props}
+          />
+          {children}
+        </>
+      ) : element === "textarea" ? (
+        <textarea
+          style={{ resize: "none" }}
+          type={type}
+          id={htmlFor}
+          className={`input ${classInput}`}
+          name={htmlFor}
+          spellCheck="false"
+          autoComplete="off"
+          {...props}
+        >
+          {children}
+        </textarea>
+      ) : (
+        <>
+          <input
+            type="checkbox"
+            id={htmlFor}
+            className={`input ${classInput}`}
+            name={htmlFor}
+            spellCheck="false"
+            autoComplete="off"
+            {...props}
+          />
+          {children}
+        </>
+      )}
+    </label>
+  );
+};
+
+export default Input;
