b2d572f1cde1a163bb5f208fca63f2f82d2ccb2f
Kurniawan Pratama <kurama.career@gmail.com>
2025-01-15 18:48:10 +0700

    DAY 4 - [NEW_FILE]: menambahkan file collection.jsx

---
+++ src/pages/07-Collection-auth/Collection.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import Container from "../../components/Container";
+import Comp_Card from "./Comp_Card";
+import "../../assets/styles/page_styles/07-Collection/collection.css";
+
+const Collection = () => {
+  const [linkCollection, setLinkCollection] = useState([
+    {
+      id: 1,
+      link: "fa8ad3",
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae, sit.",
+      updatedAt: "13/01/2024",
+      expiredAt: "13/02/2024",
+    },
+  ]);
+  return (
+    <Container id="collection" className="grid">
+      <h4 className="welcome">Selamat Datang Display Name</h4>
+      <p className="info">Tambah, Edit, Hapus, dan Bagikan Tautan yang kamu miliki</p>
+      <div className="cards">
+        {linkCollection.map((collection, index) => (
+          <Comp_Card key={index} collection={collection} />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default Collection;
