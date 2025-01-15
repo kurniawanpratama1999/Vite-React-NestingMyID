3d81b8153adf3b224ccddd55a426cb9d2f406172
Kurniawan Pratama <kurama.career@gmail.com>
2025-01-15 18:48:10 +0700

    DAY 4 - [NEW_FILE]: menambahkan file logout.jsx

---
+++ src/pages/12-Logout-Auth/Logout.jsx
@@ -0,0 +1,13 @@
+import Container from "../../components/Container";
+import '../../assets/styles/page_styles/12-Logout-auth/logout.css'
+
+const Logout = () => {
+  return (
+    <Container id="logout" className="flex">
+      <p>Anda Berhasil Logout</p>
+      <p>Jangan Lupa Kembali Lagi</p>
+    </Container>
+  );
+};
+
+export default Logout;
