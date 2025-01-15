050de68b7c16ad549c5c606c034805a9e32faebe
Kurniawan Pratama <kurama.career@gmail.com>
2025-01-15 18:48:10 +0700

    DAY 4 - [MODIFIED]: mengganti changeName.jsx agar berjalan sebagaimana mestinya

--- src/pages/05.01-ChangeName-verify/ChangeName.jsx
+++ src/pages/05.01-ChangeName-verify/ChangeName.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Container from "../../components/Container";
+import Form from "../../components/Form";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+
+import "../../assets/styles/page_styles/05.01-ChangeName/changeName.css"
 
 const ChangeName = () => {
-  return <div>ChangeName</div>;
+  return (
+    <Container id='changeName' className="grid">
+      <Form id='form-changeName' className="grid">
+        <Input htmlFor="old_name">
+          <h3>
+            Old Name <span className="star-red">*</span>
+          </h3>
+        </Input>
+        <Input htmlFor="new_name">
+          <h3>
+            New Name <span className="star-red">*</span>
+          </h3>
+        </Input>
+        <Button label="Save Changes" className="bg-green" />
+      </Form>
+    </Container>
+  );
 };
 
 export default ChangeName;
