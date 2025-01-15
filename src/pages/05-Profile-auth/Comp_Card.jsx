b8942a8d3f59fdb20d74d53755fa9354c206e4a6
Kurniawan Pratama <kurama.career@gmail.com>
2025-01-15 18:48:10 +0700

    DAY 4 - [NEW_FILE]: menambahkan file comp_card profile auth

---
+++ src/pages/05-Profile-auth/Comp_Card.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavLink } from "react-router";
+
+const Comp_Card = ({ title, desc, Icon, className = "green", redirect = null }) => {
+  return redirect ? (
+    <NavLink to={redirect} className="card-wrapper">
+      <div className={`icon-wrapper ${className}`.trim()}>
+        <Icon className="icon" />
+      </div>
+      <h6>{title}</h6>
+      <p>{desc}</p>
+    </NavLink>
+  ) : (
+    <div className="card-wrapper">
+      <div className={`icon-wrapper ${className}`.trim()}>
+        <Icon className="icon" />
+      </div>
+      <h6>{title}</h6>
+      <p>{desc}</p>
+    </div>
+  );
+};
+
+export default Comp_Card;
