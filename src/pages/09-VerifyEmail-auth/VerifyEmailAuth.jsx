dab5d2d82cdc9a6cbbe634774166e0ce864d6c45
Kurniawan Pratama <kurama.career@gmail.com>
2025-01-15 18:48:10 +0700

    DAY 4 - [NEW_FILE]: menambahkan file verifyEmalAuth.jsx

---
+++ src/pages/09-VerifyEmail-auth/VerifyEmailAuth.jsx
@@ -0,0 +1,9 @@
+import Container from "../../components/Container"
+
+const VerifyEmailAuth = () => {
+  return (
+    <Container>VerifyEmailAuth</Container>
+  )
+}
+
+export default VerifyEmailAuth
