6ce9144c91074c64cfc40b0ff2b1708b7b48d445
Kurniawan Pratama <kurama.career@gmail.com>
2025-01-15 18:48:10 +0700

    DAY 4 - [NEW_FILE]: menambahkan file requestActivation.jsx

---
+++ src/pages/05.05-RequestActivation-auth/RequestActivation.jsx
@@ -0,0 +1,12 @@
+import "../../assets/styles/page_styles/05.05-RequestActivation/requestActivation.css";
+import Container from "../../components/Container";
+const RequestActivation = () => {
+  return (
+    <Container id="requestActivation" className="grid">
+      <p>Permintaan kamu berhasil terkirim.</p>
+      <p>Silahkan check email untuk melakukan Aktivasi.</p>
+    </Container>
+  );
+};
+
+export default RequestActivation;
