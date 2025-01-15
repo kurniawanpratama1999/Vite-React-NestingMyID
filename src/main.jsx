63d305a8c03fc1694d8543b7ac8c4d5ca99451bb
Kurniawan Pratama <kurama.career@gmail.com>
2025-01-15 18:48:10 +0700

    DAY 4 - [MODIFIED]: perubahan cara penanganan routing pada main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,39 @@ import { BrowserRouter, Route, Routes } from "react-router";
 // LAYOUTS
 import Layouts from "./components/Layouts";
 
+// MIDLEWARE
+import AuthPage from "./Middleware/AuthPage";
+import VerifyPage from "./Middleware/VerifyPage";
+
 // STYLES
 import "./assets/styles/main.css";
-import CreateRoute from "./routes/CreateRoute";
+import { routeCollection } from "./routes/PageRoutes";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Layouts />}>
-        {CreateRoute}
+        {routeCollection.map((route, index) => {
+          const { isAuth, isVerify, path } = route;
+          let element = route.element;
+          if (isAuth && isVerify) {
+            element = (
+              <AuthPage>
+                <VerifyPage>{element}</VerifyPage>
+              </AuthPage>
+            );
+          }
+
+          if (isAuth && !isVerify) {
+            element = <AuthPage>{element}</AuthPage>;
+          }
+
+          if (!isAuth && isVerify) {
+            element = <VerifyPage>{element}</VerifyPage>;
+          }
+
+          return <Route key={index} path={path} element={element} />;
+        })}
       </Route>
     </Routes>
   </BrowserRouter>
