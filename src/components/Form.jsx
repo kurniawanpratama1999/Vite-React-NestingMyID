6d844be75b9a4983a7288e442b87ec54a5995ed2
Kurniawan Pratama <kurama.career@gmail.com>
2025-01-15 18:48:10 +0700

    DAY 4 - [MODIFIED]: modifikasi form.jsx untuk perubahan warna dan padding

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,7 @@ import "../assets/styles/component_styles/form.css";
 const Form = ({ children, className = "", message, ...props }) => {
   return (
     <form className={`grid relative ${className}`.trim()} {...props}>
-      <div className="message absolute">{message?.text || "message dot text"}</div>
+      <div className="message fixed tl-center-x hidden">{message?.text || "message dot text"}</div>
       {children}
     </form>
   );
